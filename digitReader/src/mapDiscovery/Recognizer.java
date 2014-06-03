ce3956069cf45db72ef77d11a69affb30c8cc726
Hazardius <hazardiusam@gmail.com>
2014-06-03 22:14:06 +0200

    change of env

--- digitReader/src/mapDiscovery/Recognizer.java
+++ digitReader/src/mapDiscovery/Recognizer.java
@@ -1,7 +1,19 @@
 package mapDiscovery;
 
+// Here will lie down sample data
+
 public class Recognizer {
+
+    private class Pair {
+
+        public Double[] dims; //dimentions
+
+        public int digit;
+    }
+
+
     static int recognize(Double[] image) {
+        List<Pair> closest = new List<Pair>();
         return 3;
     }
 }
